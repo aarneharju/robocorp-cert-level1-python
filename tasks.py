2e6e23a0269d47dd8d5fccaa00963b70567a4946
aarneharju <35223549+aarneharju@users.noreply.github.com>
2024-02-08 20:49:57 +0200

    Creating a PDF done

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ from robocorp.tasks import task
 from robocorp import browser
 from RPA.HTTP import HTTP
 from RPA.Excel.Files import Files
+from RPA.PDF import PDF
 
 @task
 def robot_spare_bin_python():
@@ -14,6 +15,7 @@ def robot_spare_bin_python():
     download_the_excel_file()
     fill_form_with_excel_data()
     collect_results()
+    export_as_pdf()
     log_out()
 
 def open_the_intranet_website():
@@ -61,3 +63,10 @@ def log_out():
     """Presses the 'Log out' button"""
     page = browser.page()  
     page.click("text=Log out")
+
+def export_as_pdf():
+    """Export the data to a pdf file"""
+    page = browser.page()
+    sales_results_html = page.locator("#sales-results").inner_html()
+    pdf = PDF()
+    pdf.html_to_pdf(sales_results_html, "output/sales_results.pdf")
